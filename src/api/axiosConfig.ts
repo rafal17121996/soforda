3c40184d27c2c61b797e5c30da27a37e87d2d056
Sebastian <seb.kan.98@gmail.com>
2024-11-12 13:57:05 +0100

    chore: Update API URL to use production endpoint

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
 //   baseURL: process.env.REACT_APP_API_URL || 'http://192.168.100.3:8000',
-  baseURL:  'http://192.168.100.33:8000',
+  baseURL:  'https://app.sofortda-gmbh.de',
   timeout: 10000, 
 });
 
